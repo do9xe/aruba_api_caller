4445dd7aad50957cb0859a6522361e0685c5ec64
Hendrik Lüth <H.L.Lueth@gmail.com>
2018-03-14 16:33:00 +0100

    cleanup of the example-script

--- example_list_vlans.py
+++ example_list_vlans.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-import json,requests,pprint
+import json,requests
 from aruba_api_caller import *
 
 session = api_session("10.0.0.1", "admin", "password", False)
 
 session.login()
 
-VLANS = session.get("configuration/container/Interfaces/_data", "/md")
+VLANS = session.get("configuration/container/Interfaces", "/md")
 
 for VLAN in VLANS["_data"]["vlan_id"]:
 	print ("VLAN-ID: " + str(VLAN["id"]))
