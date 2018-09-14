ded3c8067d55939cdb664249e130f4df08c7303f
hendrik lueth <hendrik.lueth@partner.kit.edu>
2018-09-14 15:04:06 +0200

    use format() for all strings

--- aruba_api_caller.py
+++ aruba_api_caller.py
@@ -4,7 +4,7 @@ class api_session:
   def __init__(self, api_url, username, password, check_ssl=True, verbose=False, retrys=3, retry_wait=0.5):
     self.session = None
     self.api_token = None
-    self.api_url = "https://"+ api_url +":4343/v1/"
+    self.api_url = "https://{}:4343/v1/".format(api_url)
     self.username = username
     self.password = password
     self.check_ssl = check_ssl
@@ -16,11 +16,11 @@ class api_session:
     self.session = requests.Session()
     for i in range(1,self.retrys+1):
       if self.verbose:
-        print("Verbose: login, try "+str(i))
-      response = self.session.get(self.api_url + "api/login?" + "username=" + self.username + "&password=" + self.password, verify=self.check_ssl)
+        print("Verbose: login, try {}".format(str(i)))
+      response = self.session.get("{}api/login?username={}&password={}".format(self.api_url,self.username,self.password), verify=self.check_ssl)
       login_data = json.loads(response.text)
       if self.verbose:
-        print ("Verbose: " + login_data["_global_result"]["status_str"])
+        print ("Verbose: {}".format(login_data["_global_result"]["status_str"]))
       if login_data["_global_result"]["status"] == "0":
         self.api_token = login_data["_global_result"]["UIDARUBA"]
         return
@@ -34,45 +34,46 @@ class api_session:
     logout_data = json.loads(response.text)
     self.api_token = None
     if self.verbose:
-      print ("\nVerbose: " + logout_data["_global_result"]["status_str"])
+      print ("Verbose: {}".format(logout_data["_global_result"]["status_str"]))
 
   def get(self, api_path, config_path=None):
     if self.api_token == None:
       login()
     node_path = None
     if config_path is not None:
-      node_path = "&config_path=" + config_path
-    response = self.session.get(self.api_url + api_path + "?UIDARUBA=" + self.api_token + (node_path if node_path is not None else ''))
+      node_path = "&config_path={}".format(config_path)
+    else:
+      node_path = ""
+    response = self.session.get("{}{}?UIDARUBA={}{}".format(self.api_url,api_path,self.api_token,node_path))
     data = json.loads(response.text)
     if self.verbose:
       print ("\nVerbose: " + str(data))
     return data
 
-  def post(self, api_path, config_path, data):
+  def post(self, api_path, data, config_path="/md"):
     if self.api_token == None:
       login()
-    node_path = "?config_path=" + config_path
-    response = self.session.post(self.api_url + api_path + node_path + "&UIDARUBA=" + self.api_token, json=data)
+    response = self.session.post("{}{}?UIDARUBA={}&config_path={}".format(self.api_url,api_path,self.api_token,node_path), json=data)
     data = json.loads(response.text)
     if self.verbose:
-      print ("\nVerbose: " + str(data))
+      print ("Verbose: {}".format(str(data)))
     return data
 
   def write_memory(self, config_path):
     node_path = "?config_path=" + config_path
     nothing = json.loads('{}')
-    response = self.session.post(self.api_url + "configuration/object/write_memory" + node_path + "&UIDARUBA=" + self.api_token, json=nothing)
+    response = self.session.post("{}configuration/object/write_memory{}&UIDARUBA={}".format(self.api_url,node_path,self.api_token),json=nothing)
     data = json.loads(response.text)
     if self.verbose:
-      print ("\nVerbose: " + str(data))
+      print ("Verbose: {}".format(str(data)))
     return data
 
   def cli_command(self, command):
     mod_command = command.replace(" ", "+")
-    response = self.session.get(self.api_url + "configuration/showcommand?command=" + mod_command + "&UIDARUBA=" + self.api_token)
+    response = self.session.get("{}configuration/showcommand?command={}&UIDARUBA={}".format(self.api_url,mod_command,self.api_token))
     data = json.loads(response.text)
     if self.verbose:
-      print ("\nVerbose: " + str(data))
+      print ("Verbose: {}".format(str(data)))
     return data
 
 #function to convert an Uptime from the AP-List into seconds
